2af1b073acdf7c0169d443a5dbf6ed389f9481f9
RevenkoVladislav <Vlad_reven@mail.ru>
2025-10-09 13:26:18 +0600

    add vue.js + webpack.min.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,17 @@
+import 'bootstrap';
+import '../sass/app.scss';
+/**
+ * First we will load all of this project's JavaScript dependencies which
+ * includes Vue and other libraries. It is a great starting point when
+ * building robust, powerful web applications using Vue and Laravel.
+ */
+
 import './bootstrap';
+require('./bootstrap');
+import Vue from 'vue';
+import ExampleComponent from './components/ExampleComponent.vue';
+
+const app = new Vue({
+    el: '#app',
+    components: { ExampleComponent },
+});
