6bb9099f17d8ce47072b78acb82ff745bb2620e2
RevenkoVladislav <Vlad_reven@mail.ru>
2025-10-09 19:33:36 +0600

    test v-for

--- resources/js/components/PostComponent.vue
+++ resources/js/components/PostComponent.vue
@@ -1,48 +1,59 @@
 <script>
-import SinglePostComponent from "./SinglePostComponent.vue";
+import SinglePostComponents from "./SinglePostComponent.vue";
 
 export default {
   name: "PostComponent",
-  components: {
-    SinglePostComponent: SinglePostComponent,
-  },
+  components: {SinglePostComponents},
 
   data() {
     return {
-      name: 'Вася',
-      age: 20,
-      boolean: false,
-      someNull: null,
-      arr: [],
-      someObj: {
-        personName: 'Ivan'
-      }
-    }
-  },
-
-  methods: {
-    sayHello() {
-      alert('Привет СОНЯ')
-    },
-    sayHello2() {
-      console.log('Hello-2')
-    }
-  },
-
-  computed: {
-    vasyaJob() {
-      return this.name + ' работает в булочной'
+      persons: [
+        {
+          id: 1,
+          name: "Vasya",
+          age: 20,
+          job: 'coach',
+        },
+        {
+          id: 2,
+          name: 'Sonya',
+          age: 10,
+          job: 'pupil',
+        },
+        {
+          id: 3,
+          name: 'Marina',
+          age: 30,
+          job: 'teacher',
+        }
+      ]
     }
   }
-
 }
 </script>
 
 <template>
   <div>
-    <div>{{ vasyaJob }}</div>
-    <button @click="sayHello">Hello</button>
-    <button @click="sayHello2">Hello-2</button>
+    <div>
+      <table class="table">
+        <thead>
+        <tr>
+          <th scope="col">id</th>
+          <th scope="col">Name</th>
+          <th scope="col">Age</th>
+          <th scope="col">Job</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr v-for="person in persons">
+          <th scope="row">{{ person.id }}</th>
+          <td>{{ person.name }}</td>
+          <td>{{ person.age }}</td>
+          <td>{{ person.job }}</td>
+        </tr>
+        </tbody>
+      </table>
+    </div>
   </div>
 </template>
 
